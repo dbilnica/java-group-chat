d112cb9661917d8a0c90d886f0b0e42e1cd70731
David Bilnica <david.bilnica1@gmail.com>
2024-08-16 09:37:35 +0200

    Added ServerHandling

---
+++ src/ClientHandler.java
@@ -0,0 +1,56 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientHandler implements Runnable{
+    public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private BufferReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+    private String clientUsername;
+
+    public ClientHandler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.bufferedWriter = new OutputStreamWriter(socket.getOutputStream());
+            this.bufferedReader = new InputStreamReader(socket.getInputStream());
+            this.clientUsername = bufferedReader.readLine();
+            clientHandlers.add(this);
+            broadcastMessage("SERVER: " + clientUsername + " has entered the chat!");
+
+        } catch (IOException e) {
+            closeEverything(socket, bufferedReader, bufferedWriter);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+        while(socket.isConnected()) {
+            try{
+                messageFromClient = bufferedReader.readLine();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, bufferedReader, bufferedWriter);
+                break;
+            }
+
+
+        }
+    }
+
+    public void broadcastMessage(String messageToSend) {
+        for( ClientHandler clientHandler : clientHandlers) {
+            try {
+                if(clientHandler.clientUsername.equals(clientUsername)) {
+                    clientHandler.bufferedWriter.write(messageToSend);
+                    clientHandler.bufferedWriter.newLine(); //we need
+
+                }
+            }
+        }
+    }
+}
